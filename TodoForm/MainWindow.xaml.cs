d735d20e6b0dc58b6fd4b0662938067dfd391b5c
JAC-SERVERS\reza.shalchian <reza.shalchian@a.com>
2024-02-13 12:13:20 -0500

    To do solution

---
+++ TodoForm/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TodoForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string fileName = @"..\..\mydata.txt";
+        List<Todo> todoList = new List<Todo>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadFile();
+        }
+
+        private void LoadFile()
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    string[] myData = line.Split(';');
+                    int difficulty;
+                    if (!int.TryParse(myData[2], out difficulty))
+                    {
+                        MessageBox.Show("Input string error");
+                        continue;
+                    }
+                    Todo task = new Todo(myData[0], myData[1], difficulty, myData[3]);
+                    todoList.Add(task);
+
+                    line = sr.ReadLine();
+                }
+            }
+
+            lvList.ItemsSource = todoList;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                foreach(Todo todo in todoList)
+                {
+                    sw.WriteLine(todo.ToDataString());
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtTask.Text == "")
+            {
+                MessageBox.Show("Input string error", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            DateTime? selectedDate = dpDueDate.SelectedDate;
+            if(selectedDate == null)
+            {
+                MessageBox.Show("Choose a date for the todo", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Todo todo = new Todo(txtTask.Text, selectedDate.ToString(), int.Parse(slDifficulty.Value.ToString()), combStatus.Text);
+            todoList.Add(todo);
+
+            ResetValues();
+      
+        }
+
+        private void ResetValues()
+        {
+            lvList.Items.Refresh();
+            txtTask.Clear();
+            dpDueDate.Text = "";
+            slDifficulty.Value = 1;
+            lvList.SelectedIndex = -1;
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if(lvList.SelectedIndex == -1)
+            {
+                MessageBox.Show("You need to select one item", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (txtTask.Text == "")
+            {
+                MessageBox.Show("Input string error", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Todo taskTobeUpdated = (Todo)lvList.SelectedItem;
+            taskTobeUpdated.TaskName = txtTask.Text;
+            taskTobeUpdated.Difficulty = int.Parse(slDifficulty.Value.ToString());
+            taskTobeUpdated.DueDate = dpDueDate.Text;
+            taskTobeUpdated.Status = combStatus.Text;
+
+            ResetValues();
+        }
+
+        private void lvList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            btnUpdate.IsEnabled = true;
+            btnDelete.IsEnabled = true;
+
+            var selectedItem = lvList.SelectedItem;
+
+            if(selectedItem is Todo)
+            {
+                Todo todo = (Todo)selectedItem;
+                txtTask.Text = todo.TaskName;
+                slDifficulty.Value = todo.Difficulty;
+                dpDueDate.SelectedDate = DateTime.Parse(todo.DueDate);
+                combStatus.Text = todo.Status;
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvList.SelectedIndex == -1)
+            {
+                MessageBox.Show("You need to select one item", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            Todo taskTobeDeleted = (Todo)lvList.SelectedItem;
+            if(taskTobeDeleted != null)
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure to delete the task?", "CONFIRMATION", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if(result == MessageBoxResult.Yes)
+                {
+                    todoList.Remove(taskTobeDeleted);
+                    ResetValues();
+                }
+            }
+        }
+
+        private void btnExportToFile_Click(object sender, RoutedEventArgs e)
+        {
+            //Display a SaveFileDialog so the user can save the file
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.Title = "Save the todos in a file";
+            if(saveFileDialog.ShowDialog() == true)
+            {
+                string allData = "";
+                foreach(Todo todo in todoList)
+                {
+                    allData += todo.ToString() + "\n";
+                }
+                File.WriteAllText(saveFileDialog.FileName, allData);
+            }
+        }
+    }
+}
---
+++ TodoForm/Todo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoForm
+{
+    public class Todo
+    {
+        public string TaskName { get; set; }
+        public string DueDate { get; set; }
+        public int Difficulty { get; set; }
+        public string Status { get; set; }
+
+        public Todo(string taskName, string dueDate, int difficulty, string status)
+        {
+            TaskName = taskName;
+            DueDate = dueDate;
+            Difficulty = difficulty;
+            Status = status;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} by {1}/{2}, {3}", this.TaskName, this.DueDate, this.Difficulty, this.Status);
+        }
+
+        public string ToDataString()
+        {
+            return string.Format("{0};{1};{2};{3}", this.TaskName, this.DueDate, this.Difficulty, this.Status);
+        }
+    }
+}
